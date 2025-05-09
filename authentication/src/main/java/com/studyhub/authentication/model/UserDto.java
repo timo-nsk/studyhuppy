b06d7bacd4852027af79f8632fa4f4767732ea0e
tnglf1337 <Timo.Neske@hhu.de>
2025-05-09 22:11:14 +0200

    created utilit classes for when user data is retrieved

---
+++ authentication/src/main/java/com/studyhub/authentication/model/UserDto.java
@@ -0,0 +1,10 @@
+package com.studyhub.authentication.model;
+
+import java.util.UUID;
+
+public record UserDto(UUID userId,
+                      String mail,
+                      String username,
+                      Boolean notificationSubscription,
+                      Integer semester) {
+}
---
+++ authentication/src/main/java/com/studyhub/authentication/model/UserMapper.java
@@ -0,0 +1,12 @@
+package com.studyhub.authentication.model;
+
+public class UserMapper {
+
+    public static UserDto toDto(AppUser user) {
+        return new UserDto(user.getUserId(),
+                user.getMail(),
+                user.getUsername(),
+                user.getNotificationSubscription(),
+                user.getSemester());
+    }
+}
