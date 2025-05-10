87f8f11df2fce4c043ea22eead75cc5440fffa52
tnglf1337 <Timo.Neske@hhu.de>
2025-05-10 09:33:18 +0200

    user data renders in template

--- frontend/src/app/user-profile/user.ts
+++ frontend/src/app/user-profile/user.ts
@@ -1,10 +1,7 @@
 export interface User {
-  id : number;
   userId : string;
   mail : string;
-  username : string;
-  password : string;
+  username : string;password : string;
   notificationSubscription : boolean;
-  acceptedAgb : boolean;
   semester : number;
 }
