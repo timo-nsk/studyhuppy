ff042d5aa7e03f85cd278bb6a828b793e5d32da2
tnglf1337 <Timo.Neske@hhu.de>
2025-05-14 08:44:01 +0200

    created backend endpoint to retrieve all kartei sets for the user,

--- kartei/src/main/java/com/studyhub/kartei/adapter/web/controller/StapelApiController.java
+++ kartei/src/main/java/com/studyhub/kartei/adapter/web/controller/StapelApiController.java
@@ -1,12 +1,15 @@
 package com.studyhub.kartei.adapter.web.controller;
 
+import com.studyhub.jwt.JWTService;
 import com.studyhub.kartei.domain.model.Stapel;
 import com.studyhub.kartei.service.application.StapelService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,9 +17,11 @@ import java.util.Map;
 public class StapelApiController {
 
 	private final StapelService stapelService;
+	private final JWTService jwtService;
 
-	public StapelApiController(StapelService stapelService) {
+	public StapelApiController(StapelService stapelService, JWTService jwtService) {
 		this.stapelService = stapelService;
+		this.jwtService = jwtService;
 	}
 
 	@Api
@@ -33,9 +38,19 @@ public class StapelApiController {
 		return responseMap;
 	}
 
+	// TODO: muss noch per username
 	@AngularApi
 	@GetMapping("/sets-available")
 	public ResponseEntity<Boolean> setsAvailable() {
 		return ResponseEntity.ok(stapelService.areKarteiSetsAvailable());
+	}
+
+	@AngularApi
+	@GetMapping("/get-all-stapel-by-username")
+	public ResponseEntity<List<StapelDashboardDto>> getSetsByUsername(HttpServletRequest request) {
+		String username = jwtService.extractUsernameFromHeader(request);
+		List<Stapel> stapel = stapelService.findByUsername(username);
+		List<StapelDashboardDto> res = stapelService.prepareDashboardInfo(stapel);
+		return ResponseEntity.ok(res);
 	}
 }
