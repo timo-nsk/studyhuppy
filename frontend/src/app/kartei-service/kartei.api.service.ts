7cde02905bc37284d1844d684a2d52141cce8a3c
tnglf1337 <Timo.Neske@hhu.de>
2025-05-14 12:39:23 +0200

    created api connection to backend kartei services

---
+++ frontend/src/app/kartei-service/kartei.api.service.ts
@@ -0,0 +1,24 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {HeaderService} from '../header.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KarteiApiService {
+  BASE_API_URL : string = "http://localhost:9081/api"
+
+  http = inject(HttpClient)
+  headerService = inject(HeaderService)
+
+  checkSetsAvailable() : Observable<boolean>{
+    const headers = this.headerService.createAuthHeader()
+    return this.http.get<boolean>(this.BASE_API_URL + "/sets-available", {headers})
+  }
+
+  getAllStapelByUsername() : Observable<any>{
+    const headers = this.headerService.createAuthHeader()
+    return this.http.get<any>(this.BASE_API_URL + "/get-all-stapel-by-username", {headers})
+  }
+}
