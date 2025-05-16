da53eb2ecefe497e82ad21fc0e550b9c2a35ea59
tnglf1337 <Timo.Neske@hhu.de>
2025-05-16 17:14:52 +0200

    stapel-details now renders the page of the stapel with its related data

--- frontend/src/app/kartei-service/stapel-details/stapel-details.component.ts
+++ frontend/src/app/kartei-service/stapel-details/stapel-details.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Stapel} from '../domain';
+import {KarteiApiService} from '../kartei.api.service';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-stapel-details',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './stapel-details.component.html',
+  standalone: true,
   styleUrl: './stapel-details.component.scss'
 })
-export class StapelDetailsComponent {
+export class StapelDetailsComponent implements OnInit{
+
+  stapel : Stapel = {}
+
+  route = inject(ActivatedRoute)
+  karteiService = inject(KarteiApiService)
+
+  ngOnInit(): void {
+    let stapelId: string | null = ''
+    this.route.paramMap.subscribe(params => { stapelId = params.get('fachId'); });
+
+    this.karteiService.getStapelByFachId(stapelId).subscribe({
+      next: (data : Stapel) => {
+        this.stapel = data
+      }
+    })
+  }
+
+
 
 }
