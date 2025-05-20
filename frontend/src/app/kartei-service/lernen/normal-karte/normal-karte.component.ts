030111c0acd07f63697638cb53aa99cc040a425a
tnglf1337 <Timo.Neske@hhu.de>
2025-05-20 18:43:43 +0200

    render karteikarte data on ui and implemented logic to reveals answers karteikarte normal

--- frontend/src/app/kartei-service/lernen/normal-karte/normal-karte.component.ts
+++ frontend/src/app/kartei-service/lernen/normal-karte/normal-karte.component.ts
@@ -1,19 +1,41 @@
-import {Component, Input} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {Stapel} from '../../domain';
 
 @Component({
   selector: 'app-normal-karte',
   imports: [
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './normal-karte.component.html',
   standalone: true,
-  styleUrls: ['./normal-karte.component.scss', '../lernen.component.scss']
+  styleUrls: ['./normal-karte.component.scss', '../lernen.component.scss', '../../../button.scss']
 })
-export class NormalKarteComponent {
+export class NormalKarteComponent implements OnInit{
 
-  @Input() frage : string | undefined;
-  @Input() antwort : string | undefined;
-  @Input() hideAntwort : boolean = true;
+  @Input() stapelData: Stapel | undefined
+  @Input() btnDataList : any[] | undefined
+  @Input() kartenIndex!: number
+  @Input() hideAntwort : boolean = true
+  @Input() hideAntwortBtn : boolean = true
+  @Input() hideBtnGroup : boolean = false
 
+  frage: string | undefined;
+  antwort: string | undefined;
+
+  ngOnInit(): void {
+    this.frage = this.stapelData?.karteikarten?.[this.kartenIndex].frage
+    this.antwort = this.stapelData?.karteikarten?.[this.kartenIndex].antwort
+  }
+
+  revealAntwort() {
+    this.hideAntwort = !this.hideAntwort
+    this.hideAntwortBtn = false
+    this.hideBtnGroup = true
+  }
+
+  updateKarteikarte(i: number, $event: MouseEvent) {
+    console.log("updating normal karteikarte")
+  }
 }
