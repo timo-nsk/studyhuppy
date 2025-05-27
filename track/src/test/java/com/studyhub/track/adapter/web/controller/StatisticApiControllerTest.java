392196afac1a382e546c465f81e5a7ace106bebb
Timo Neske <neske.timo93@gmail.com>
2025-05-27 16:58:39 +0200

    tested endpoints of StatisticApiController for authenticated and not authenticated users to be available/not available

--- track/src/test/java/com/studyhub/track/adapter/web/controller/StatisticApiControllerTest.java
+++ track/src/test/java/com/studyhub/track/adapter/web/controller/StatisticApiControllerTest.java
@@ -1,16 +1,19 @@
 package com.studyhub.track.adapter.web.controller;
 
+import com.studyhub.jwt.JWTService;
 import com.studyhub.track.adapter.security.JwtAuthFilter;
 import com.studyhub.track.adapter.security.RedirectEntryPoint;
 import com.studyhub.track.adapter.security.SecurityConfig;
 import com.studyhub.track.adapter.web.controller.api.StatisticApiController;
 import com.studyhub.track.application.service.ModulEventService;
+import com.studyhub.track.application.service.ModulService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,12 +28,70 @@ public class StatisticApiControllerTest {
 	MockMvc mvc;
 
 	@MockitoBean
+	ModulService modulService;
+
+	@MockitoBean
 	ModulEventService modulEventService;
 
 	@MockitoBean
-	RedirectEntryPoint redirectEntryPoint;
+	JWTService jwtService;
 
-	@MockitoBean
-	JwtAuthFilter filter;
+	@Test
+	@DisplayName("Alle API-Endpunkte sind für nicht authentifizierte Benutzer zugänglich")
+	void test_1() throws Exception {
+		mvc.perform(get("/statistics/api/v1/chart"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-total-study-time"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-average-study-time-per-day"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-total-study-time-per-semester"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-number-active-module"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-number-not-active-module"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-max-studied-modul"))
+				.andExpect(status().isForbidden());
+
+		mvc.perform(get("/statistics/api/v1/get-min-studied-modul"))
+				.andExpect(status().isForbidden());
+
+	}
+
+	@Test
+	@DisplayName("API-Endpunkte sind für authentifizierte Benutzer zugänglich")
+	@WithMockUser(username = "testuser", roles = "USER")
+	void test_2() throws Exception {
+		mvc.perform(get("/statistics/api/v1/chart"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-total-study-time"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-average-study-time-per-day"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-total-study-time-per-semester"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-number-active-module"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-number-not-active-module"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-max-studied-modul"))
+				.andExpect(status().isOk());
+
+		mvc.perform(get("/statistics/api/v1/get-min-studied-modul"))
+				.andExpect(status().isOk());
+	}
 
 }
