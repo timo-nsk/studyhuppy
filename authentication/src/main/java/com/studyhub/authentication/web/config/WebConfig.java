453546e27719c7a2df66fd9b99d25d9c15597e69
tnglf1337 <Timo.Neske@hhu.de>
2025-05-30 19:43:21 +0200

    update WebConfig.java

--- authentication/src/main/java/com/studyhub/authentication/web/config/WebConfig.java
+++ authentication/src/main/java/com/studyhub/authentication/web/config/WebConfig.java
@@ -14,7 +14,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
+                        .allowedOrigins
+                        (
+                                "http://localhost:4200",
+                                "http://127.0.0.1:4200",
+                                "http://49.12.242.124:8080",
+                                "http://49.12.242.124:4200"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization");;
