43e16615e0162189d601a7ef14eaa8a15404c8e9
tnglf1337 <Timo.Neske@hhu.de>
2025-05-30 20:41:31 +0200

    test

--- authentication/src/main/java/com/studyhub/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/studyhub/authentication/AuthenticationApplication.java
@@ -1,5 +1,7 @@
 package com.studyhub.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class AuthenticationApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(AuthenticationApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("TEST");
+		log.info("WAS BUILD CORRECT");
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
