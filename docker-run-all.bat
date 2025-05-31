56735324501a3fb9dd586ca20b16c9cd8d1ec36d
Timo Neske <neske.timo93@gmail.com>
2025-05-31 16:13:30 +0200

    updated scripts

--- docker-run-all.bat
+++ docker-run-all.bat
@@ -10,23 +10,23 @@ for /f "tokens=1-3 delims=:.," %%a in ("%time%") do (
 
 echo [%date% !hh!:!mm!:!ss!] Starte Studyhub-Services...
 echo [%date% !hh!:!mm!:!ss!] Starte Mail-Service...
-docker compose --env-file mail/.env -f mail/docker-compose.yaml up -d
+docker compose --env-file mail/.env -f mail/docker-compose.yaml up -d --build
 echo [%date% !hh!:!mm!:!ss!] Mail-Service gestartet.
 
 echo [%date% !hh!:!mm!:!ss!] Starte Kartei-Service...
-docker compose --env-file kartei/.env -f kartei/docker-compose.yaml up -d
+docker compose --env-file kartei/.env -f kartei/docker-compose.yaml up -d --build
 echo [%date% !hh!:!mm!:!ss!] Kartei-Service gestartet.
 
 echo [%date% !hh!:!mm!:!ss!] Starte Track-Service...
-docker compose --env-file track/.env -f track/docker-compose.yaml up -d
+docker compose --env-file track/.env -f track/docker-compose.yaml up -d --build
 echo [%date% !hh!:!mm!:!ss!] Track-Service gestartet.
 
 echo [%date% !hh!:!mm!:!ss!] Starte Authentication-Service...
-docker compose --env-file authentication/.env -f authentication/docker-compose.yaml up -d
+docker compose --env-file authentication/.env -f authentication/docker-compose.yaml up -d --build
 echo [%date% !hh!:!mm!:!ss!] Authentication-Service gestartet.
 
 echo [%date% !hh!:!mm!:!ss!] Starte Actuator-Service...
-docker compose --env-file actuator/.env -f actuator/docker-compose.yaml up -d
+docker compose --env-file actuator/.env -f actuator/docker-compose.yaml up -d --build
 echo [%date% !hh!:!mm!:!ss!] Actuator-Service gestartet.
 
 echo [%date% !hh!:!mm!:!ss!] Alle Studyhub-Services sind gestartet.
