e84d3a700d972bc9d86a50163db310078be96eef
tnglf1337 <Timo.Neske@hhu.de>
2025-06-01 19:32:14 +0200

    removed unnecessary files

--- track/out/test/resources/com/studyhub/track/statistic/insert_statistics.sql
+++
@@ -1,4 +0,0 @@
-insert into statistic (id, date, fach_id) values
-                            (1541651, '2024-01-10', 'dd5ce63e-f0d9-4b5e-935c-1b40989ab2b4'),
-                            (2546546, '2024-03-08', 'ed5ce63e-f0d9-4b5e-935c-1b40989ab2b4'),
-                            (3546543, '2024-04-08', 'fd5ce63e-f0d9-4b5e-935c-1b40989ab2b4');
