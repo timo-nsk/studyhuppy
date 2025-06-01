f547682e761bd1aa741322558303806a15b993ab
tnglf1337 <Timo.Neske@hhu.de>
2025-06-01 17:30:43 +0200

    update file

--- frontend/src/app/guard/authentication.guard.ts
+++ frontend/src/app/guard/authentication.guard.ts
@@ -1,13 +1,17 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import {LoginStatusService} from '../auth-service/login-service/login-status.service';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
+  let loginStatusService = inject(LoginStatusService)
   const router = inject(Router);
   const token = localStorage.getItem("auth_token");
 
   if (token != null) {
+    loginStatusService.login()
     return true;
   } else {
+    loginStatusService.logout()
     return router.parseUrl('/login');
   }
 };
