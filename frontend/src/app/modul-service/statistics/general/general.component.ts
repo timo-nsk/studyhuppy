2695ae8417ffd96163847dadceacea3db71d98d8
tnglf1337 <Timo.Neske@hhu.de>
2025-06-04 17:47:32 +0200

    add logging to class

--- frontend/src/app/modul-service/statistics/general/general.component.ts
+++ frontend/src/app/modul-service/statistics/general/general.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {StatisticApiService} from '../statistic.service';
 import {TimeFormatPipe} from '../../module/time-format.pipe';
 import {NgFor} from '@angular/common';
+import {LoggingService} from '../../../logging.service';
 
 @Component({
   selector: 'app-general',
@@ -11,6 +12,8 @@ import {NgFor} from '@angular/common';
   styleUrl: './general.component.scss'
 })
 export class GeneralComponent implements OnInit{
+  log = new LoggingService("GeneralComponent", "modul-service")
+  service = inject(StatisticApiService)
 
   totalStudyTime: number = 0;
   totalStudyTimePerSemester: { key: number, value: number }[] = [];
@@ -20,38 +23,74 @@ export class GeneralComponent implements OnInit{
   maxStudiedModul: string = '';
   minStudiedModul: string = '';
 
-  constructor(private service: StatisticApiService, private timeFormat : TimeFormatPipe) {}
-
   ngOnInit(): void {
-    this.service.getTotalStudyTime().subscribe(value => {
-      this.totalStudyTime = value;
+    this.service.getTotalStudyTime().subscribe({
+      next: (data) => {
+        this.totalStudyTime = data;
+        this.log.info(`got total study time: ${data}`)
+      },
+      error: (err) => {
+        this.log.error(`error getting total study time. reason: ${err}`)
+      }
     });
 
-    this.service.getTotalStudyTimeperSemester().subscribe(data => {
-      this.totalStudyTimePerSemester = Object.entries(data).map(([key, value]) => ({
-        key: Number(key),
-        value: value
-      }));
+    this.service.getTotalStudyTimeperSemester().subscribe({
+      next: (data) => {
+        this.totalStudyTimePerSemester = Object.entries(data).map(([key, value]) => ({
+          key: Number(key),
+          value: value
+        }));
+        this.log.info(`got total study time per semester: ${data}`)
+      },
+      error: (err) => {
+        this.log.error(`error getting total study time per semester. reason: ${err}`)
+      }
     });
 
-    this.service.getDurchschnittlicheLernzeitProTag().subscribe(data => {
-      this.averageStudyTimePerDay = data
+    this.service.getDurchschnittlicheLernzeitProTag().subscribe({
+      next: data => {
+        this.averageStudyTimePerDay = data
+        this.log.info(`got average lernzeit per day: ${data}`)
+      },
+      error: err => {
+        this.log.info(`error getting average lernzeit per day. reason: ${err}`)
+      }
     })
 
-    this.service.getNumberActiveModules().subscribe(value => {
-      this.numberActiveModules = value;
+    this.service.getNumberActiveModules().subscribe({
+      next: data => {
+        this.log.info(`got no. of active module: ${data}`)
+      },
+      error: err => {
+        this.log.error(`error getting no. of active module: ${err}`)
+      }
     });
 
-    this.service.getNumberNotActiveModules().subscribe(value => {
-      this.numberNotActiveModules = value;
+    this.service.getNumberNotActiveModules().subscribe({
+      next: data => {
+        this.log.info(`got no. of not active module: ${data}`)
+      },
+      error: err => {
+        this.log.error(`error getting no. of not active module: ${err}`)
+      }
     });
 
-    this.service.getMaxStudiedModul().subscribe(value => {
-      this.maxStudiedModul = value;
+    this.service.getMaxStudiedModul().subscribe({
+      next: data => {
+        this.log.info(`got max studied modul: ${data}`)
+      },
+      error: err => {
+        this.log.error(`error getting max studied modul: ${err}`)
+      }
     });
 
-    this.service.getMinStudiedModul().subscribe(value => {
-      this.minStudiedModul = value;
+    this.service.getMinStudiedModul().subscribe({
+      next: data => {
+        this.log.info(`got min studied modul: ${data}`)
+      },
+      error: err => {
+        this.log.error(`error getting min studied modul: ${err}`)
+      }
     });
   }
 }
