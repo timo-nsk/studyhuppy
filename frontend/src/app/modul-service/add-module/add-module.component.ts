0171fd8275b04d4ea754484ec1be1773cf86b322
tnglf1337 <Timo.Neske@hhu.de>
2025-06-04 09:58:04 +0200

    add logging to class, update snackbar

--- frontend/src/app/modul-service/add-module/add-module.component.ts
+++ frontend/src/app/modul-service/add-module/add-module.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {ModuleApiService} from '../module/module-api.service';
 import {HttpClient} from '@angular/common/http';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {SnackbarService} from '../../snackbar.service';
+import {LoggingService} from '../../logging.service';
 
 @Component({
   selector: 'app-add-module',
@@ -13,31 +15,30 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   styleUrls: ['./add-module.component.scss', './color.scss', '../../forms.scss', '../../general.scss']
 })
 export class AddModuleComponent {
+  sb = inject(SnackbarService)
+  service = inject(ModuleApiService)
+  http = inject(HttpClient)
+  log = new LoggingService("AddModuleComponent", "modul-service")
   lerntageCheckbox : boolean = false
-
-  constructor(private service : ModuleApiService, private http : HttpClient, private snackbar : MatSnackBar) {
-  }
 
   newModulForm = this.initForm()
 
   onSubmit() : void {
-    const form = this.newModulForm.value
-
-
+    if(this.newModulForm.valid) {
+      this.log.debug("newModulForm data VALID")
+      const form = this.newModulForm.value
       this.service.postFormData(form).subscribe({
         next: () => {
-          this.snackbar.open("Modul erfolgreich erstellt", "dismiss", {
-            duration: 4000
-          })
+          this.sb.openInfo("Modul erfolgreich erstellt")
         },
         error: (err) => {
           console.log(err)
-          this.snackbar.open("Sie können keine weiteren Module erstellen", "dismiss", {
-            duration: 4000
-          })
+          this.sb.openError("Sie können keine weiteren Module erstellen")
         }
       })
-
+    } else {
+      this.log.debug("newModulForm data INVALID")
+    }
   }
 
   toggleLerntageCheckbox() {
