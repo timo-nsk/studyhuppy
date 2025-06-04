fb12cf6e6ce68339dc4f49970fdfbb1343e78f3e
tnglf1337 <Timo.Neske@hhu.de>
2025-06-04 17:32:50 +0200

    add logging to class, update snackbar

--- frontend/src/app/modul-service/statistics/statistic.service.ts
+++ frontend/src/app/modul-service/statistics/statistic.service.ts
@@ -3,16 +3,19 @@ import {HttpClient} from '@angular/common/http';
 import {HeaderService} from '../../header.service';
 import {Observable} from 'rxjs';
 import { environment } from '../../../environments/environment'
+import {LoggingService} from '../../logging.service';
 
 @Injectable({
   providedIn: "root"
 })
 export class StatisticApiService {
   BASE_API_URL : string = environment.modulServiceUrl
+  log = new LoggingService("StatisticApiService", "modul-service")
   http = inject(HttpClient)
   headerService = inject(HeaderService)
 
   getTotalStudyTime(): Observable<number> {
+    this.log.info("Try get total study time...")
     return this.http.get<number>(
       this.BASE_API_URL + "/get-total-study-time",
       {
@@ -22,6 +25,7 @@ export class StatisticApiService {
   }
 
   getDurchschnittlicheLernzeitProTag(): Observable<number> {
+    this.log.info("Try get average study time per day...")
     return this.http.get<number>(
       this.BASE_API_URL + "/get-average-study-time-per-day",
       {
@@ -31,12 +35,14 @@ export class StatisticApiService {
   }
 
   getTotalStudyTimeperSemester(): Observable<{ [key: number]: number }> {
+    this.log.info("Try get total study time per semester...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get<{ [key: number]: number }>(
       this.BASE_API_URL + "/get-total-study-time-per-semester", { headers});
   }
 
   getNumberActiveModules() : Observable<string> {
+    this.log.info("Try get no. of active module...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get(this.BASE_API_URL + "/get-number-active-module",
       {
@@ -47,6 +53,7 @@ export class StatisticApiService {
   }
 
   getNumberNotActiveModules() : Observable<string> {
+    this.log.info("Try get no. of not active module...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get(this.BASE_API_URL + "/get-number-not-active-module",
       {
@@ -57,6 +64,7 @@ export class StatisticApiService {
   }
 
   getMaxStudiedModul() : Observable<string> {
+    this.log.info("Try get max studied modul...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get(this.BASE_API_URL + "/get-max-studied-modul",
       {
@@ -67,6 +75,7 @@ export class StatisticApiService {
   }
 
   getMinStudiedModul() : Observable<string> {
+    this.log.info("Try get min studied module...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get(this.BASE_API_URL + "/get-min-studied-modul",
       {
@@ -77,6 +86,7 @@ export class StatisticApiService {
   }
 
   getChartLastDays() : Observable<{ [date: string]: { modulName: string; secondsLearned: string }[] }> {
+    this.log.info("Try get chart data for last days...")
     const headers = this.headerService.createAuthHeader()
     return this.http.get<{ [date: string]: { modulName: string; secondsLearned: string }[] }>(this.BASE_API_URL + "/chart", { headers })
   }
