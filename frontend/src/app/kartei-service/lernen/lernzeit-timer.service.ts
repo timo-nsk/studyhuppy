9c6088fd71efc2dfa6263f99a879d62528e0912f
Timo Neske <neske.timo93@gmail.com>
2025-06-04 20:34:26 +0200

    added logging

--- frontend/src/app/kartei-service/lernen/lernzeit-timer.service.ts
+++ frontend/src/app/kartei-service/lernen/lernzeit-timer.service.ts
@@ -1,15 +1,17 @@
+import {LoggingService} from '../../logging.service';
+
 export class LernzeitTimer {
+  log = new LoggingService("LernzeitTimer", "kartei-service")
+
   lernzeitGesamt : number | undefined;
   lernzeitCurrentKarte : number | undefined;
   overallTimerId: any;
   currentTimerId: any;
   sessionRunning : boolean = false
-  private _anzahlKarten: number | undefined;
 
-  constructor(anzahlKarten : number | undefined) {
+  constructor() {
     //If the object is initialized, it will start its overall timers
     if(!this.isSessionRunning) {
-      this._anzahlKarten = anzahlKarten;
       this.lernzeitGesamt = 0
       this.lernzeitCurrentKarte = 0
       this.sessionRunning = true
@@ -19,23 +21,23 @@ export class LernzeitTimer {
 
   startOverallTimer(): void {
     this.overallTimerId= setInterval(() => {
-      this.lernzeitGesamt!++;
-    }, 1000);
-    this.sessionRunning = true;
-    console.log("starting OVERALL timer, id:" + this.overallTimerId);
+      this.lernzeitGesamt!++
+    }, 1000)
+    this.sessionRunning = true
+    this.log.info(`starting OVERALL timer, id: ${this.overallTimerId}`)
   }
 
   clearOverallTimer() {
-    clearInterval(this.overallTimerId);
-    this.sessionRunning = false;
-    console.log("-- cleared OVERALL timer");
+    clearInterval(this.overallTimerId)
+    this.sessionRunning = false
+    this.log.info(`cleared OVERALL timer, id: ${this.overallTimerId}`)
   }
 
   startCurrentKarteTimer(): void {
     this.currentTimerId = setInterval(() => {
-      this.lernzeitCurrentKarte!++;
-      }, 1000);
-    console.log("- started CURRENT timer, id:" + this.currentTimerId);
+      this.lernzeitCurrentKarte!++
+      }, 1000)
+    this.log.info(`starting CURRENT timer, id: ${this.currentTimerId}`)
   }
 
   getLernzeitGesamt() : number {
@@ -51,7 +53,7 @@ export class LernzeitTimer {
   }
 
   clearCurrentTimer() {
-    console.log("-- cleared CURRENT timer, id:" + this.currentTimerId);
+    this.log.info(`cleared CURRENT timer, id: ${this.currentTimerId}`)
     this.lernzeitCurrentKarte = 0
     clearInterval(this.currentTimerId);
   }
