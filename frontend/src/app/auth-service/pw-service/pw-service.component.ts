d7bd5b35f4650524e44259cb5cca3ccd7d0f1c5d
tnglf1337 <Timo.Neske@hhu.de>
2025-06-05 20:33:51 +0200

    added logging

--- frontend/src/app/auth-service/pw-service/pw-service.component.ts
+++ frontend/src/app/auth-service/pw-service/pw-service.component.ts
@@ -3,19 +3,17 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {NgIf} from '@angular/common';
 import {AuthApiService} from '../auth.service';
 import {RouterLink} from '@angular/router';
+import {LoggingService} from '../../logging.service';
 
 @Component({
   selector: 'app-pw-service',
-  imports: [
-    ReactiveFormsModule,
-    NgIf,
-    RouterLink
-  ],
+  imports: [ReactiveFormsModule, NgIf, RouterLink],
   templateUrl: './pw-service.component.html',
   standalone: true,
   styleUrls: ['./pw-service.component.scss', '../../general.scss', '../../color.scss', '../../links.scss', '../../button.scss']
 })
 export class PwServiceComponent {
+  log = new LoggingService("PwServiceComponent", "auth-service")
 
   authService = inject(AuthApiService)
 
@@ -27,27 +25,28 @@ export class PwServiceComponent {
   })
 
   sendPwResetRequest() {
-    const data = this.pwResetForm.value
-
-    if (this.pwResetForm.invalid) {
-      this.pwResetForm.markAllAsTouched();
-      return;
-    }
-    console.log("hiiiii")
-
-    this.authService.pwReset(data).subscribe({
-      next: (response) => {
-        this.emailFound = true;
-        this.emailNotFound = false;
-      },
-      error: (error) => {
-        if (error.status === 404) {
-          this.emailNotFound = true;
-          this.emailFound = false;
-        } else {
-          console.error("Ein Fehler ist aufgetreten:", error);
+    if (this.pwResetForm.valid) {
+      this.log.debug("form data VALID")
+      const data = this.pwResetForm.value
+      this.authService.pwReset(data).subscribe({
+        next: (response) => {
+          this.emailFound = true;
+          this.emailNotFound = false;
+          this.log.debug("password succesfully reset")
+        },
+        error: (error) => {
+          if (error.status === 404) {
+            this.emailNotFound = true;
+            this.emailFound = false;
+            this.log.error(`error resetting password. reason: no email available`)
+          } else {
+            this.log.error(`error resetting password. reason: ${error}`)
+          }
         }
-      }
-    });
+      });
+    } else {
+      this.log.debug("form data INVALID")
+      this.pwResetForm.markAllAsTouched();
+    }
   }
 }
