77ec925d20365fb418a1ec2c3ae7b1f1fee3ecd4
tnglf1337 <Timo.Neske@hhu.de>
2025-06-08 01:14:38 +0200

    update prod config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const DOMAIN : string = "49.12.242.124"
 
 export const environment = {
   production: true,
-  authServiceUrl: `http://${DOMAIN}:9084/api/v1`,
+  authServiceUrl: `https://${DOMAIN}:9084/api/v1`,
   modulServiceUrl: `http://${DOMAIN}:9080/api/v1`,
   karteiServiceUrl: `http://${DOMAIN}:9081/api/v1`,
   actuatorServiceUrl: `http://${DOMAIN}:9085/api/v1`
