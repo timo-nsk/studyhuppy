32b10c5eb458c372da93efc5271b9af7246276ec
tnglf1337 <Timo.Neske@hhu.de>
2025-06-09 00:37:17 +0200

    update cors

--- kartei/src/main/java/com/studyhub/kartei/adapter/config/security/SecurityConfig.java
+++ kartei/src/main/java/com/studyhub/kartei/adapter/config/security/SecurityConfig.java
@@ -49,17 +49,25 @@ public class SecurityConfig {
 		log.info("Configuring CorsConfigurationSource...");
 
 		CorsConfiguration config = new CorsConfiguration();
+
 		config.setAllowedOrigins(List.of(
 				"http://localhost:4200",
 				"http://127.0.0.1:4200",
 				"http://127.0.0.1:8080",
 				"http://49.12.242.124:8080",
-				"http://49.12.242.124:4200"
+				"http://49.12.242.124:4200",
+				"https://49.12.242.124",
+				"https://49.12.242.124:8443",
+				"https://49.12.242.124:9084",
+				"https://studyhuppy.de",
+				"https://www.studyhuppy.de"
 		));
+
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
-		config.setExposedHeaders(List.of("Authorization"));
+		config.setExposedHeaders(List.of("Authorization", "Content-Type"));
 		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
