9bcc8f60050bcb39cffb05db6b97b73c3b15fac3
tnglf1337 <Timo.Neske@hhu.de>
2025-06-08 02:25:54 +0200

    add cors debug filter

--- authentication/src/main/java/com/studyhub/authentication/config/JwtAuthFilter.java
+++ authentication/src/main/java/com/studyhub/authentication/config/JwtAuthFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Order(1)
 public class JwtAuthFilter extends OncePerRequestFilter {
 
 
