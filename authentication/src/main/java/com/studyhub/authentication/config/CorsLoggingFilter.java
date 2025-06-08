dc6cfdddeacd673cff08bd4b571e55fef2f37e2a
tnglf1337 <Timo.Neske@hhu.de>
2025-06-08 02:17:58 +0200

    add cors debug filter

---
+++ authentication/src/main/java/com/studyhub/authentication/config/CorsLoggingFilter.java
@@ -0,0 +1,24 @@
+package com.studyhub.authentication.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CorsLoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            System.out.println("CORS Preflight Request to: " + request.getRequestURI());
+            System.out.println("Origin: " + request.getHeader("Origin"));
+        }
+        filterChain.doFilter(request, response);
+    }
+}
+
