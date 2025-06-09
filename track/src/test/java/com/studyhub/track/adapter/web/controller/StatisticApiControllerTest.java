3ef7ea523839a91ba46cd13a58f25c2ec85bcbfe
timoneske <Timo.Neske@hhu.de>
2025-06-09 17:45:20 +0200

    fix tests, all green

--- track/src/test/java/com/studyhub/track/adapter/web/controller/StatisticApiControllerTest.java
+++ track/src/test/java/com/studyhub/track/adapter/web/controller/StatisticApiControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(StatisticApiController.class)
 @Import(SecurityConfig.class)
+@ActiveProfiles("application-dev.yaml")
 public class StatisticApiControllerTest {
 
 	@Autowired
@@ -36,28 +38,28 @@ public class StatisticApiControllerTest {
 	@Test
 	@DisplayName("Alle API-Endpunkte sind für nicht authentifizierte Benutzer zugänglich")
 	void test_1() throws Exception {
-		mvc.perform(get("/statistics/api/v1/chart"))
+		mvc.perform(get("/api/modul/v1/chart"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-total-study-time"))
+		mvc.perform(get("/api/modul/v1/get-total-study-time"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-average-study-time-per-day"))
+		mvc.perform(get("/api/modul/v1/get-average-study-time-per-day"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-total-study-time-per-semester"))
+		mvc.perform(get("/api/modul/v1/get-total-study-time-per-semester"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-number-active-module"))
+		mvc.perform(get("/api/modul/v1/get-number-active-module"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-number-not-active-module"))
+		mvc.perform(get("/api/modul/v1/get-number-not-active-module"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-max-studied-modul"))
+		mvc.perform(get("/api/modul/v1/get-max-studied-modul"))
 				.andExpect(status().isForbidden());
 
-		mvc.perform(get("/statistics/api/v1/get-min-studied-modul"))
+		mvc.perform(get("/api/modul/v1/get-min-studied-modul"))
 				.andExpect(status().isForbidden());
 
 	}
@@ -66,28 +68,28 @@ public class StatisticApiControllerTest {
 	@DisplayName("API-Endpunkte sind für authentifizierte Benutzer zugänglich")
 	@WithMockUser(username = "testuser", roles = "USER")
 	void test_2() throws Exception {
-		mvc.perform(get("/statistics/api/v1/chart"))
+		mvc.perform(get("/api/modul/v1/chart"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-total-study-time"))
+		mvc.perform(get("/api/modul/v1/get-total-study-time"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-average-study-time-per-day"))
+		mvc.perform(get("/api/modul/v1/get-average-study-time-per-day"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-total-study-time-per-semester"))
+		mvc.perform(get("/api/modul/v1/get-total-study-time-per-semester"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-number-active-module"))
+		mvc.perform(get("/api/modul/v1/get-number-active-module"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-number-not-active-module"))
+		mvc.perform(get("/api/modul/v1/get-number-not-active-module"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-max-studied-modul"))
+		mvc.perform(get("/api/modul/v1/get-max-studied-modul"))
 				.andExpect(status().isOk());
 
-		mvc.perform(get("/statistics/api/v1/get-min-studied-modul"))
+		mvc.perform(get("/api/modul/v1/get-min-studied-modul"))
 				.andExpect(status().isOk());
 	}
 
