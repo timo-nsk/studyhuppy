8a079fe339e05eb4ce2f8f3d453f90995c44e4c4
timoneske <Timo.Neske@hhu.de>
2025-06-09 23:20:21 +0200

    added validation for root initialization

--- mindmap/src/main/java/com/studyhub/mindmap/domain/model/MindmapNode.java
+++ mindmap/src/main/java/com/studyhub/mindmap/domain/model/MindmapNode.java
@@ -4,12 +4,13 @@ import lombok.Data;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Relationship;
-
+import org.yaml.snakeyaml.util.Tuple;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@DirectedAcyclicTree
 public class MindmapNode {
     @Id
     @GeneratedValue
@@ -25,7 +26,7 @@ public class MindmapNode {
     private List<MindmapNode> childNodes;
 
     public MindmapNode(UUID modulId, String title, String text, NodeType nodeType, NodeRole nodeRole, String username) {
-        this.nodeId = null;
+        this.nodeId = UUID.randomUUID();
         this.modulId = modulId;
         this.title = title;
         this.text = text;
@@ -45,12 +46,50 @@ public class MindmapNode {
         this.childNodes = new ArrayList<>();
     }
 
+    /**
+     * This factory method must be used when creating a new mindmap.
+     * A root mindmap node has a module ID and a node ID.
+     * The module ID is required for linking to the module in the module service.
+     * The root node can additionally be identified by the username.
+     *
+     * @param modulId ID of the module, which has been created in the module service. Can be 'null'
+     * @param title Title of the node
+     * @param text Accompanying information for the node
+     * @param nodeType Type of the node
+     * @param username Username of the user
+     * @return Root of the mindmap
+     * @throws IllegalRootConsistencyException when parameters are invalid
+     */
+    @RootFactoryMethod
     public static MindmapNode initRootNode(UUID modulId, String title, String text, NodeType nodeType, String username) {
+        Tuple<List<String>, Boolean> resTuple = checkConsistency(modulId, title, nodeType, username);
+
+        if (!resTuple._2()) {
+            throw new IllegalRootConsistencyException(resTuple._1().stream().toString());
+        }
+
         return new MindmapNode(modulId, title, text, nodeType, NodeRole.ROOT, username);
     }
 
+    /**
+     * A child mindmap node does not have a module ID.
+     * It can be found in the tree by its node ID.
+     *
+     * @param title Title of the node
+     * @param text Accompanying information for the node
+     * @param nodeType Type of the node
+     * @return Child for a mindmap
+     * @throws IllegalRootConsistencyException when parameters are invalid
+     */
+    @ChildFactoryMethod
     public static MindmapNode initChildNode(String title, String text, NodeType nodeType) {
-        return new MindmapNode(null, null, title, text, nodeType, NodeRole.CHILD);
+        Tuple<List<String>, Boolean> resTuple = checkConsistency(UUID.randomUUID(), title, nodeType, "dummy");
+
+        if (!resTuple._2()) {
+            throw new IllegalRootConsistencyException(resTuple._1().stream().toString());
+        }
+
+        return new MindmapNode(UUID.randomUUID(), null, title, text, nodeType, NodeRole.CHILD);
     }
 
 
@@ -170,5 +209,26 @@ public class MindmapNode {
                 ", nodeType=" + nodeType +
                 ", childNodes=" + childNodes +
                 '}';
+    }
+
+    public static Tuple<List<String>, Boolean> checkConsistency(UUID modulId, String title, NodeType nodeType, String username) {
+        List<String> mustBeSet = new ArrayList<>();
+
+        if(modulId == null || modulId.toString().isEmpty()) {
+            mustBeSet.add("modulId");
+        }
+        if(title == null || title.isEmpty()) {
+            mustBeSet.add("title");
+        }
+        if(nodeType == null || nodeType.name().isEmpty()) {
+            mustBeSet.add("nodeType");
+        }
+        if(username == null || username.isEmpty()) {
+            mustBeSet.add("username");
+        }
+
+        boolean isValid = mustBeSet.isEmpty();
+
+        return new Tuple<>(mustBeSet, isValid);
     }
 }
--- mindmap/src/test/java/com/studyhub/mindmap/MindmapNodeTest.java
+++ mindmap/src/test/java/com/studyhub/mindmap/MindmapNodeTest.java
@@ -1,13 +1,17 @@
 package com.studyhub.mindmap;
 
+import com.studyhub.mindmap.domain.model.IllegalRootConsistencyException;
 import com.studyhub.mindmap.domain.model.MindmapNode;
 import com.studyhub.mindmap.domain.model.NodeType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.util.Tuple;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MindmapNodeTest {
 
@@ -57,12 +61,152 @@ public class MindmapNodeTest {
     @DisplayName("Einem Elternknoten kann ein Kindknoten hinzugefügt werden")
     void test5() {
         UUID parentId = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
-        UUID childId = UUID.fromString("550e8401-e29b-41d4-a716-446655440000");
         MindmapNode newChild = MindmapNode.initChildNode("im new", "bla",NodeType.SUBJECT);
 
         mindmap.addChildNode(parentId, newChild);
 
         assertThat(mindmap.size()).isEqualTo(8);
-        assertThat(mindmap.findChildNode(childId).getTitle()).isEqualTo("im new");
+        assertThat(mindmap.findChildNode(newChild.getNodeId()).getTitle()).isEqualTo("im new");
+    }
+
+    @Test
+    @DisplayName("Fehlender Parameterwert für 'modulId' returned das korrekte Tupel als Antwort")
+    void test6()  {
+        UUID modulId1 = null;
+        String titel = "titel";
+        String username = "username";
+        NodeType nodeType = NodeType.SUBJECT;
+
+        Tuple<List<String>, Boolean> res1 = MindmapNode.checkConsistency(modulId1, titel, nodeType, username);
+
+        assertThat(res1._2()).isFalse();
+        assertThat(res1._1().contains("modulId")).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Fehlender Parameterwert für 'title' returned das korrekte Tupel als Antwort")
+    void test7()  {
+        UUID modulId = UUID.randomUUID();
+        String titel1 = null;
+        String titel2 = "";
+        String username = "username";
+        NodeType nodeType = NodeType.SUBJECT;
+
+        Tuple<List<String>, Boolean> res1 = MindmapNode.checkConsistency(modulId, titel1, nodeType, username);
+        Tuple<List<String>, Boolean> res2 = MindmapNode.checkConsistency(modulId, titel2, nodeType, username);
+
+        assertThat(res1._2()).isFalse();
+        assertThat(res2._2()).isFalse();
+        assertThat(res1._1().contains("title")).isEqualTo(true);
+        assertThat(res2._1().contains("title")).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Fehlender Parameterwert für 'nodeType' returned das korrekte Tupel als Antwort")
+    void test8()  {
+        UUID modulId = UUID.randomUUID();
+        String titel = "titel";
+        String username = "username";
+        NodeType nodeType1 = null;
+
+        Tuple<List<String>, Boolean> res1 = MindmapNode.checkConsistency(modulId, titel, nodeType1, username);
+
+        assertThat(res1._2()).isFalse();
+        assertThat(res1._1().contains("nodeType")).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Fehlender Parameterwert für 'username' returned das korrekte Tupel als Antwort")
+    void test9()  {
+        UUID modulId = UUID.randomUUID();
+        String titel = "titel";
+        String username1 = null;
+        String username2 = "";
+        NodeType nodeType = NodeType.SUBJECT;
+
+        Tuple<List<String>, Boolean> res1 = MindmapNode.checkConsistency(modulId, titel, nodeType, username1);
+        Tuple<List<String>, Boolean> res2 = MindmapNode.checkConsistency(modulId, titel, nodeType, username2);
+
+        assertThat(res1._2()).isFalse();
+        assertThat(res2._2()).isFalse();
+        assertThat(res1._1().contains("username")).isEqualTo(true);
+        assertThat(res2._1().contains("username")).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Ale Parameterwerte fehlen und das korrekte Tupel als Antwort")
+    void test10()  {
+        UUID modulId = null;
+        String titel1 = null;
+        String titel2 = "";
+        String username1 = null;
+        String username2 = "";
+        NodeType nodeType = null;
+
+        Tuple<List<String>, Boolean> res1 = MindmapNode.checkConsistency(modulId, titel1, nodeType, username1);
+        Tuple<List<String>, Boolean> res2 = MindmapNode.checkConsistency(modulId, titel2, nodeType, username2);
+
+        assertThat(res1._2()).isFalse();
+        assertThat(res2._2()).isFalse();
+        assertThat(res1._1().contains("username")).isEqualTo(true);
+        assertThat(res1._1().contains("modulId")).isEqualTo(true);
+        assertThat(res1._1().contains("nodeType")).isEqualTo(true);
+        assertThat(res1._1().contains("title")).isEqualTo(true);
+        assertThat(res2._1().contains("username")).isEqualTo(true);
+        assertThat(res2._1().contains("modulId")).isEqualTo(true);
+        assertThat(res2._1().contains("nodeType")).isEqualTo(true);
+        assertThat(res2._1().contains("title")).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Wenn mit ungültigen Parameter 'modulId' eine RootMindmapNode erstellt wird, wird eine Exception geworfen")
+    void test11()  {
+        UUID modulId = null;
+        String titel = "titel";
+        NodeType nodeType = NodeType.SUBJECT;
+        String username = "peter";
+
+        assertThrows(IllegalRootConsistencyException.class, () -> {
+            MindmapNode root = MindmapNode.initRootNode(null, titel, "info", nodeType, username);
+        });
+    }
+
+    @Test
+    @DisplayName("Wenn mit ungültigen Parameter 'title' eine RootMindmapNode erstellt wird, wird eine Exception geworfen")
+    void test12()  {
+        UUID modulId = UUID.randomUUID();
+        String titel = null;
+        NodeType nodeType = NodeType.SUBJECT;
+        String username = "peter";
+
+        assertThrows(IllegalRootConsistencyException.class, () -> {
+            MindmapNode root = MindmapNode.initRootNode(modulId, titel, "info", nodeType, username);
+        });
+    }
+
+    @Test
+    @DisplayName("Wenn mit ungültigen Parameter 'nodeType' eine RootMindmapNode erstellt wird, wird eine Exception geworfen")
+    void test13()  {
+        UUID modulId = UUID.randomUUID();
+        String titel = "title";
+        NodeType nodeType = null;
+        String username = "peter";
+
+        assertThrows(IllegalRootConsistencyException.class, () -> {
+            MindmapNode root = MindmapNode.initRootNode(modulId, titel, "info", nodeType, username);
+        });
+    }
+
+    @Test
+    @DisplayName("Wenn mit ungültigen Parameter 'username' eine RootMindmapNode erstellt wird, wird eine Exception geworfen")
+    void test14()  {
+        UUID modulId = UUID.randomUUID();
+        String titel = "title";
+        NodeType nodeType = NodeType.SUBJECT;
+        String username = null;
+
+        assertThrows(IllegalRootConsistencyException.class, () -> {
+            MindmapNode root = MindmapNode.initRootNode(modulId, titel, "info", nodeType, username);
+        });
     }
 }
