aaa0a1d7dec98e3686746eb4334752a504da1471
timoneske <Timo.Neske@hhu.de>
2025-06-11 01:57:42 +0200

    create HealthController.java

--- kartei/src/main/java/com/studyhub/kartei/adapter/actuator/HealthController.java
+++ kartei/src/main/java/com/studyhub/kartei/adapter/actuator/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/kartei/v1")
 public class HealthController {
 
 	private final StapelService stapelService;
