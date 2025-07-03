197289e4208acd8398bde11ad953368ee7d946f2
Timo Neske <Timo.Neske@hhu.de>
2025-07-04 00:28:05 +0200

    created new microservice for system tests

---
+++ systemtests/src/test/java/com/studyhuppy/systemtests/SystemtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhuppy.systemtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemtestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
