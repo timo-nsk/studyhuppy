70fab421f60ad4611c9ca6493eb705b7fff3989e
Timo Neske <Timo.Neske@hhu.de>
2025-07-08 18:51:37 +0200

    refactored TimeConverterTest.java

--- track/src/test/java/com/studyhub/track/service/TimeConverterTest.java
+++ track/src/test/java/com/studyhub/track/service/TimeConverterTest.java
@@ -3,7 +3,11 @@ package com.studyhub.track.service;
 import com.studyhub.track.application.service.TimeConverter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.time.LocalTime;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,84 +22,34 @@ public class TimeConverterTest {
 		tc = new TimeConverter();
 	}
 
-	@Test
-	@DisplayName("10:30 werden korrekt in Sekunden konvertiert")
-	void test_1() {
-		String time = "01:30";
-
-		int converted = tc.timeToSeconds(time);
-
-		assertThat(converted).isEqualTo(5400);
-	}
-
-	@Test
-	@DisplayName("25:60 wird als ungültige Eingabe erkannt.")
-	void test_2() {
-		String time = "25:60";
-
-		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+	@Nested
+	class ValidConversion {
+		@ParameterizedTest
+		@CsvSource(value="01:30, 5400")
+		void test_01(String time, int expected) {
 			int converted = tc.timeToSeconds(time);
-		});
+			assertThat(converted).isEqualTo(expected);
+		}
 
-		assertEquals("hh and mm out of range", thrown.getMessage());
+		@Test
+		@DisplayName("Der Zeit-String '11:11' wird korrekt in ein LocalDate umgewandelt")
+		void test_7() {
+			String time = "11:11";
+
+			LocalTime resultTime = TimeConverter.getLocalTimeFromString(time);
+
+			assertThat(resultTime.getHour()).isEqualTo(11);
+			assertThat(resultTime.getMinute()).isEqualTo(11);
+		}
 	}
 
-	@Test
-	@DisplayName("23:-50 wird als ungültige Eingabe erkannt.")
-	void test_3() {
-		String time = "23:-50";
-
-		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-			int converted = tc.timeToSeconds(time);
-		});
-
-		assertEquals("hh and mm out of range", thrown.getMessage());
-	}
-
-	@Test
-	@DisplayName("-23:49 wird als ungültige Eingabe erkannt.")
-	void test_4() {
-		String time = "-23:49";
-
-		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-			int converted = tc.timeToSeconds(time);
-		});
-
-		assertEquals("hh and mm out of range", thrown.getMessage());
-	}
-
-	@Test
-	@DisplayName("-23:-49 wird als ungültige Eingabe erkannt.")
-	void test_5() {
-		String time = "-23:-49";
-
-		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-			int converted = tc.timeToSeconds(time);
-		});
-
-		assertEquals("hh and mm out of range", thrown.getMessage());
-	}
-
-	@Test
-	@DisplayName("-23-49 wird als ungültige Eingabe erkannt.")
-	void test_6() {
-		String time = "-23-49";
-
-		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-			int converted = tc.timeToSeconds(time);
-		});
-
-		assertEquals("hh and mm out of range", thrown.getMessage());
-	}
-
-	@Test
-	@DisplayName("Der Zeit-String '11:11' wird korrekt in ein LocalDate umgewandelt")
-	void test_7() {
-		String time = "11:11";
-
-		LocalTime resultTime = TimeConverter.getLocalTimeFromString(time);
-
-		assertThat(resultTime.getHour()).isEqualTo(11);
-		assertThat(resultTime.getMinute()).isEqualTo(11);
+	@Nested
+	class InvalidConversion {
+		@ParameterizedTest
+		@CsvSource(value="25:60, 23:-50, -23:49, -23:-49")
+		void test_01(String time) {
+			IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> tc.timeToSeconds(time));
+			assertEquals("hh and mm out of range", thrown.getMessage());
+		}
 	}
 }
