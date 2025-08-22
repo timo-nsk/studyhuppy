8c97f38c566b1b55aaede4010c9f466fb9c4f167
Timo Neske <Timo.Neske@hhu.de>
2025-08-22 07:10:55 +0200

    updated files

--- frontend/src/app/modul-service/session/session.component.ts
+++ frontend/src/app/modul-service/session/session.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LernblockComponent} from './lernblock/lernblock.component';
+import {PausenblockComponent} from './pausenblock/pausenblock.component';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+
 
 @Component({
   selector: 'app-session',
-  imports: [],
+  imports: [LernblockComponent, PausenblockComponent, FormsModule, NgForOf],
   templateUrl: './session.component.html',
+  standalone: true,
   styleUrl: './session.component.scss'
 })
-export class SessionComponent {
+export class SessionComponent implements OnInit{
+  anzahlBloecke : number = 2;
 
+  ngOnInit(): void {
+    console.log(this.anzahlBloecke)
+  }
+
+  print() : void {
+    console.log(this.anzahlBloecke)
+  }
 }
