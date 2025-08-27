ebaa1e079e92589360cca29d94e72dbb104be4b6
Timo Neske <neske.timo93@gmail.com>
2025-08-27 17:14:54 +0200

    added annotation

--- track/src/main/java/com/studyhub/track/adapter/db/lernplan/LernplanRepositoryImpl.java
+++ track/src/main/java/com/studyhub/track/adapter/db/lernplan/LernplanRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.studyhub.track.adapter.db.lernplan;
 
 import com.studyhub.track.application.service.LernplanRepository;
 import com.studyhub.track.domain.model.lernplan.Lernplan;
+import org.springframework.stereotype.Repository;
 
 import static com.studyhub.track.adapter.db.lernplan.LernplanMapper.toDomain;
 import static com.studyhub.track.adapter.db.lernplan.LernplanMapper.toDto;
 
+@Repository
 public class LernplanRepositoryImpl implements LernplanRepository {
 
 	private final LernplanDao lernplanDao;
