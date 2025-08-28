17eb1d95235407116bdebc2e84cb6dbd1995a166
Timo Neske <neske.timo93@gmail.com>
2025-08-28 12:33:30 +0200

    implemented backend api point to save the sent lernplan data

--- track/src/main/java/com/studyhub/track/adapter/web/controller/api/LernplanApiController.java
+++ track/src/main/java/com/studyhub/track/adapter/web/controller/api/LernplanApiController.java
@@ -1,16 +1,37 @@
 package com.studyhub.track.adapter.web.controller.api;
 
+import com.studyhub.track.adapter.web.controller.request.dto.LernplanRequest;
+import com.studyhub.track.application.JWTService;
+import com.studyhub.track.application.service.LernplanService;
+import com.studyhub.track.domain.model.lernplan.Lernplan;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/plan/v1")
 public class LernplanApiController {
+
+	private LernplanService lernplanService;
+	private JWTService jwtService;
+
+	public LernplanApiController(LernplanService lernplanService, JWTService jwtService) {
+		this.lernplanService = lernplanService;
+		this.jwtService = jwtService;
+	}
+
 	@PostMapping("/create")
-	public ResponseEntity<Void> createLernplan() {
-		// TODO: Implement the creation of a Lernplan
-		return ResponseEntity.ok().build();
+	public ResponseEntity<Void> createLernplan(@RequestBody LernplanRequest lernplanRequest, HttpServletRequest httpRequest) {
+		Lernplan lernplan = lernplanRequest.toEntity(jwtService.extractUsernameFromHeader(httpRequest));
+		boolean success = lernplanService.saveLernplan(lernplan);
+
+		if (success) {
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.internalServerError().build();
+		}
 	}
 }
