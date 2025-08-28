280bd19a90ea9ce3aa9d97f1fc4973f44711a82e
Timo Neske <neske.timo93@gmail.com>
2025-08-28 12:11:54 +0200

    implement method to convert request object to entity object

--- track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/LernplanRequest.java
+++ track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/LernplanRequest.java
@@ -1,11 +1,48 @@
 package com.studyhub.track.adapter.web.controller.request.dto;
 
+import com.studyhub.track.domain.model.lernplan.Lernplan;
+import com.studyhub.track.domain.model.lernplan.Tag;
 import com.studyhub.track.domain.model.lernplan.TagDto;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.UUID;
 
 public record LernplanRequest(
 		String lernplanTitel,
 		List<TagDto> tage
 ) {
+	public Lernplan toEntity(String username) {
+		List<Tag> tagesListe = tage.stream()
+				.filter(e -> !e.session().equals("none"))
+				.map(dtoObj -> {
+
+					DayOfWeek tag = null;
+
+					switch(dtoObj.weekday()) {
+						case "Montags" -> tag = DayOfWeek.MONDAY;
+						case "Dienstags" -> tag = DayOfWeek.TUESDAY;
+						case "Mittwochs" -> tag = DayOfWeek.WEDNESDAY;
+						case "Donnerstags" -> tag = DayOfWeek.THURSDAY;
+						case "Freitags" -> tag = DayOfWeek.FRIDAY;
+						case "Samstags" -> tag = DayOfWeek.SATURDAY;
+						case "Sonntags" -> tag = DayOfWeek.SUNDAY;
+					}
+
+					LocalTime beginn = LocalTime.parse(dtoObj.beginn());
+
+
+					return new Tag(tag, beginn, UUID.randomUUID());
+				})
+				.toList();
+
+		return Lernplan.builder()
+				.fachId(UUID.randomUUID())
+				.username(username)
+				.titel(lernplanTitel)
+				.tagesListe(tagesListe)
+				.isActive(false)
+				.build();
+	}
 }
