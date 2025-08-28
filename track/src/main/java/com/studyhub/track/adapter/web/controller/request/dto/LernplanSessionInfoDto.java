79f64e3eba9dc4804c9ed9a64534ddeb0741cadc
Timo Neske <neske.timo93@gmail.com>
2025-08-29 01:17:13 +0200

    created LernplanSessionInfoDto

---
+++ track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/LernplanSessionInfoDto.java
@@ -0,0 +1,11 @@
+package com.studyhub.track.adapter.web.controller.request.dto;
+
+import com.studyhub.track.domain.model.session.Block;
+import java.util.List;
+
+public record LernplanSessionInfoDto(
+		String weekday,
+		String sessionId,
+		List<Block> blocks
+) {
+}
