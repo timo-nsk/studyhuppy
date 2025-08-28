9468105919029920b1f2288cfdf5836605cb8f21
Timo Neske <neske.timo93@gmail.com>
2025-08-29 01:17:35 +0200

    update file

--- track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/LernplanResponse.java
+++ track/src/main/java/com/studyhub/track/adapter/web/controller/request/dto/LernplanResponse.java
@@ -1,11 +1,11 @@
 package com.studyhub.track.adapter.web.controller.request.dto;
 
-import com.studyhub.track.domain.model.lernplan.TagDto;
+import com.studyhub.track.domain.model.session.Session;
 
 import java.util.List;
 
 public record LernplanResponse(
-		String titel,
-		List<TagDto> tagesListe
+		String lernplanTitel,
+		List<LernplanSessionInfoDto> sessionList
 ) {
 }
