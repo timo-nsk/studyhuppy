e578758b9c24583945bf21ce7d6d08bf18c603e6
Timo Neske <neske.timo93@gmail.com>
2025-08-29 01:22:06 +0200

    implement logic and styling to render the active lernplan on ui

--- frontend/src/app/plan-service/plan-service.component.ts
+++ frontend/src/app/plan-service/plan-service.component.ts
@@ -1,14 +1,18 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {PlanApiService} from './plan-api.service';
-import {LernplanRequest} from './plan-domain';
+import {LernplanResponse, LernplanSessionInfoDto} from './plan-domain';
+import {TimeFormatPipe} from '../modul-service/module/time-format.pipe';
 
 @Component({
   selector: 'app-plan-service',
   imports: [
     NgIf,
-    RouterLink
+    RouterLink,
+    NgForOf,
+    TimeFormatPipe,
+    NgClass
   ],
   templateUrl: './plan-service.component.html',
   standalone: true,
@@ -16,15 +20,19 @@ import {LernplanRequest} from './plan-domain';
 })
 export class PlanServiceComponent implements OnInit{
   planApiService = inject(PlanApiService)
-  activeLernplan : LernplanRequest = {} as LernplanRequest;
+  activeLernplan : LernplanResponse = {} as LernplanResponse;
   activePlanExists: boolean = false;
+  weekdayInfos: LernplanSessionInfoDto[] = [];
+  weekContainerDisabled: boolean[] = [true, true, true, true, true, true, true];
 
   ngOnInit(): void {
+    this.disableWeekContainer()
     this.planApiService.getActiveLernplan().subscribe({
       next: (response) => {
         this.activeLernplan = response;
+        this.initWeekdayInfos();
         this.activePlanExists = true;
-        console.log(this.activeLernplan)
+        console.log(this.activeLernplan.lernplanTitel)
       },
       error: (status) => {
         if (status == 404) {
@@ -33,5 +41,66 @@ export class PlanServiceComponent implements OnInit{
         }
       }
     });
+  }
+
+  private initWeekdayInfos() {
+    let sessionList = this.activeLernplan.sessionList;
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Montags') || {
+      weekday: "Montags",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Dienstags') || {
+      weekday: "Dienstags",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Mittwochs') || {
+      weekday: "Mittwochs",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Donnerstags') || {
+      weekday: "Donnerstags",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Freitags') || {
+      weekday: "Freitags",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Samstags') || {
+      weekday: "Samstags",
+      sessionId: "",
+      blocks: []
+    });
+    this.weekdayInfos.push(sessionList.find(session => session.weekday === 'Sonntags') || {
+      weekday: "Sonntags",
+      sessionId: "",
+      blocks: []
+    });
+  }
+
+  totalLearningTimePerDay(weekdayInfo: LernplanSessionInfoDto) {
+    let total = 0;
+    for (const block of weekdayInfo.blocks) {
+      total += block.lernzeitSeconds + block.pausezeitSeconds;
+    }
+    return total;
+  }
+
+  disableWeekContainer() {
+    let todayDate = new Date();
+    let weekday = todayDate.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+
+    // Umrechnung: Montag = 0, ..., Sonntag = 6
+    weekday = (weekday + 6) % 7;
+
+    for (let i = 0; i < this.weekContainerDisabled.length; i++) {
+      this.weekContainerDisabled[i] = (i !== weekday);
+    }
+
+    console.log(this.weekContainerDisabled);
   }
 }
