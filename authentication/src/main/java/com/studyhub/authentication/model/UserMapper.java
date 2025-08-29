8b88d93f62d31e802e0df6a72388906596ed6ef2
Timo Neske <neske.timo93@gmail.com>
2025-08-29 19:39:49 +0200

    update file

--- authentication/src/main/java/com/studyhub/authentication/model/UserMapper.java
+++ authentication/src/main/java/com/studyhub/authentication/model/UserMapper.java
@@ -3,11 +3,12 @@ package com.studyhub.authentication.model;
 public class UserMapper {
 
     public static UserDto toDto(AppUser user) {
+        String filename = user.getProfilbildPath().split("/")[3];
         return new UserDto(user.getUserId(),
                 user.getMail(),
                 user.getUsername(),
                 user.getNotificationSubscription(),
                 user.getSemester(),
-                user.getProfilbildPath());
+                filename);
     }
 }
