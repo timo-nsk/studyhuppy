5992642d85d731c391346f8970ffc0c40a8d3ea9
Timo Neske <neske.timo93@gmail.com>
2025-08-29 19:41:03 +0200

    update avatar component

--- frontend/src/app/user-profile/avatar/avatar.component.ts
+++ frontend/src/app/user-profile/avatar/avatar.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {UserApiService} from '../user.service';
 
 @Component({
   selector: 'app-avatar',
-  imports: [
-    NgOptimizedImage,
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
-export class AvatarComponent {
+export class AvatarComponent implements OnInit{
+  userService  = inject(UserApiService)
+  userData : any;
+  profilbildUrl: any;
 
+  ngOnInit(): void {
+    this.getUserData()
+  }
+
+  getUserData() {
+    this.userService.getUserData().subscribe({
+      next: (data) => {
+        this.userData = data;
+        this.getProfilbild()
+      },
+      error: (err) => {
+        console.error('Fehler beim Laden:', err);
+      }
+    });
+  }
+
+  getProfilbild() {
+    this.userService.getProfilbild(this.userData).subscribe( {
+      next: blob => {
+        console.log(blob)
+        const reader = new FileReader();
+        reader.onload = () => this.profilbildUrl = reader.result as string;
+        reader.readAsDataURL(blob);
+      },
+      error: err => {
+        console.log(err)
+      }
+    });
+  }
 }
