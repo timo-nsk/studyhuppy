12e969f8839e5ad79687efcf63fdb3880d32e064
Timo Neske <neske.timo93@gmail.com>
2025-08-30 22:54:11 +0200

    add styling to plan-service.component.html

--- frontend/src/app/plan-service/plan-service.component.ts
+++ frontend/src/app/plan-service/plan-service.component.ts
@@ -1,13 +1,26 @@
-import {Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {SnackbarService} from '../snackbar.service';
 
 
 @Component({
   selector: 'app-plan-service',
-  imports: [],
+  imports: [
+    NgIf,
+    RouterLink
+  ],
   templateUrl: './plan-service.component.html',
   standalone: true,
   styleUrls: ['./plan-service.component.scss', '../general.scss', '../button.scss']
 })
 export class PlanServiceComponent {
+  snackbarService = inject(SnackbarService)
+  titel = "blub"
 
+  deleteLernplan() {
+    // delete lernplan by id
+    console.log("deleted")
+    this.snackbarService.openSuccess(`Lernplan "${this.titel}" gelöscht`)
+  }
 }
