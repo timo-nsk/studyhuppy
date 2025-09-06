dd8c5a9ceded2295d5d256bc4b579ae3f91f8db9
Timo Neske <neske.timo93@gmail.com>
2025-09-06 23:37:12 +0200

    style home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {jwtDecode} from 'jwt-decode';
+import {HomeService, UserServiceInformation} from './home.service';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgIf, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss', '../general.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  homeService = inject(HomeService)
 
+  username: string = "default";
+  userServiceInformation : UserServiceInformation = {} as UserServiceInformation
+
+  ngOnInit(): void {
+    this.getUsername()
+    this.homeService.gatherUserServiceInformation().subscribe(usi => {
+      this.userServiceInformation = usi;
+    });
+  }
+
+  getUsername() {
+    const token = localStorage.getItem("auth_token");
+
+    if (token != null) {
+      const decoded = jwtDecode(token)
+      this.username =  decoded.sub ?? 'none'
+    }
+  }
+
+  hasModule() {
+    return this.userServiceInformation.hasModule
+  }
+
+  hasLernSessions() {
+    return this.userServiceInformation.hasLernSessions
+  }
+
+  hasLernplan() {
+    return this.userServiceInformation.hasLernplan
+  }
+
+  hasKarteikartenstapel() {
+    return this.userServiceInformation.hasKarteikartenStapel
+  }
 }
