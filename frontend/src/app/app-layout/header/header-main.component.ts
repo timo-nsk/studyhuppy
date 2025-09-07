5934b43dc672903284347d916bf7b2fed3bc8ad2
Timo Neske <neske.timo93@gmail.com>
2025-09-07 23:09:44 +0200

    refactored header component

--- frontend/src/app/app-layout/header/header-main.component.ts
+++ frontend/src/app/app-layout/header/header-main.component.ts
@@ -2,10 +2,11 @@ import {Component, inject} from '@angular/core';
 import {NgIf} from '@angular/common';
 import  { LoginStatusService } from '../../auth-service/login-service/login-status.service'
 import {AvatarComponent} from '../../user-profile/avatar/avatar.component';
+import {LogoComponent} from '../logo/logo.component';
 
 @Component({
   selector: 'app-header-main',
-  imports: [NgIf, AvatarComponent],
+  imports: [NgIf, AvatarComponent, LogoComponent],
   templateUrl: './header-main.component.html',
   standalone: true,
   styleUrls: ['./header-main.component.scss', '../../general.scss', '../../color.scss', '../../links.scss', '../../button.scss']
