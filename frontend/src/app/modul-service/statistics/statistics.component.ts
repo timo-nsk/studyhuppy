6500698b3597295b4cbd3b619f3e2067bb943af3
Timo Neske <neske.timo93@gmail.com>
2025-09-10 00:25:22 +0200

    update styling

--- frontend/src/app/modul-service/statistics/statistics.component.ts
+++ frontend/src/app/modul-service/statistics/statistics.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-statistics',
-  imports: [
-    RouterLink,
-    RouterOutlet
+  imports: [RouterLink, RouterOutlet, NgClass
   ],
   templateUrl: './statistics.component.html',
   standalone: true,
   styleUrls: ['./statistics.component.scss', '../../links.scss', '../../color.scss']
 })
 export class StatisticsComponent {
+  isActive : boolean[] = [true, false]
 
+  activatePaginationitem(i: number) {
+    for (let j = 0; j < this.isActive.length; j++) {
+      if (j == i) {
+        this.isActive[j] = true
+      } else {
+        this.isActive[j] = false
+      }
+    }
+  }
 }
