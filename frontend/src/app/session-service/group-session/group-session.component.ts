03672bc762990a7faee304ea5e6a04e1cf7b8adb
Timo Neske <neske.timo93@gmail.com>
2025-09-11 19:40:35 +0200

    create group-session component

---
+++ frontend/src/app/session-service/group-session/group-session.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-session',
+  imports: [],
+  templateUrl: './group-session.component.html',
+  styleUrl: './group-session.component.scss'
+})
+export class GroupSessionComponent {
+
+}
