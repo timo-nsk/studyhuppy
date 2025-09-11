305efedb51fc5d9fdc0e12a16a7c0ea855a25e3e
Timo Neske <neske.timo93@gmail.com>
2025-09-11 18:02:08 +0200

    update stylign and add new card element

--- frontend/src/app/session-service/session.component.ts
+++ frontend/src/app/session-service/session.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-session',
-  imports: [FormsModule, ReactiveFormsModule, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, MatIcon],
   templateUrl: './session.component.html',
   standalone: true,
   styleUrls: ['./session.component.scss', '../general.scss', '../button.scss']
