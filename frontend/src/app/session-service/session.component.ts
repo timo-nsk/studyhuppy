6eac9a82c342b2efb2244a728b69fefd9748d111
Timo Neske <neske.timo93@gmail.com>
2025-09-11 23:23:54 +0200

    remove import

--- frontend/src/app/session-service/session.component.ts
+++ frontend/src/app/session-service/session.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
-import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-session',
-  imports: [FormsModule, ReactiveFormsModule, RouterLink, MatIcon],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './session.component.html',
   standalone: true,
   styleUrls: ['./session.component.scss', '../general.scss', '../button.scss']
