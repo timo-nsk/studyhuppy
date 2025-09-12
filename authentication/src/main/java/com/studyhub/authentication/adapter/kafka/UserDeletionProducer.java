4b126f0cf4e99567040a4887c44d5e71d37ec114
Timo Neske <neske.timo93@gmail.com>
2025-09-12 22:31:16 +0200

    change sent object type to UserDto

--- authentication/src/main/java/com/studyhub/authentication/adapter/kafka/UserDeletionProducer.java
+++ authentication/src/main/java/com/studyhub/authentication/adapter/kafka/UserDeletionProducer.java
@@ -1,9 +1,14 @@
 package com.studyhub.authentication.adapter.kafka;
 
+import com.studyhub.kafka.dto.UserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
 
 @Service
 public class UserDeletionProducer {
@@ -16,8 +21,15 @@ public class UserDeletionProducer {
 		this.kafkaTemplate = kafkaTemplate;
 	}
 
-	public void sendMessage(String userId) {
+	public void sendMessage(String username, String userId) {
 		LOGGER.info(String.format("Producing message for user deletion with user-id: %s", userId));
-		kafkaTemplate.send("user-deletion", userId);
+		UserDto dto = new UserDto(UUID.fromString(userId), username);
+
+		Message<UserDto> message = MessageBuilder
+				.withPayload(dto)
+				.setHeader(KafkaHeaders.TOPIC, "user-deletion")
+				.build();
+
+		kafkaTemplate.send(message);
 	}
 }
