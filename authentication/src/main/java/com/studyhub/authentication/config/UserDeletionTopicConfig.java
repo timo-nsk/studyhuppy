716ca236d655d53e4f46afe516dc4d9afbb8c1fc
Timo Neske <neske.timo93@gmail.com>
2025-09-12 22:30:43 +0200

    renamed file

--- authentication/src/main/java/com/studyhub/authentication/config/UserDeletionTopicConfig.java
+++ authentication/src/main/java/com/studyhub/authentication/config/UserDeletionTopicConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-public class KafkaTopicConfig {
+public class UserDeletionTopicConfig {
 
 	@Bean
 	public NewTopic topicConfig() {
