27aa3c73e4caa9cba5f20e2cbaf2748fb9552a8e
Timo Neske <neske.timo93@gmail.com>
2025-09-16 19:44:39 +0200

    add method setPomodoroValues to select all desired values in the html-select-elements of block.component.html

--- frontend/src/app/session-service/block/block.component.ts
+++ frontend/src/app/session-service/block/block.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input} from '@angular/core';
+import {Component, effect, Input} from '@angular/core';
 import {NgForOf} from '@angular/common';
 import {Block} from '../session-domain';
 import {Modul} from '../../modul-service/module/domain';
 import {BlockManager} from './block-manager.service';
+import {FormsModule} from '@angular/forms';
+import {PomodoroSignalService} from '../create/pomodoro-signal.service';
 
 @Component({
   selector: 'Lernblock',
-  imports: [NgForOf],
+  imports: [NgForOf, FormsModule],
   templateUrl: './block.component.html',
   standalone: true,
   styleUrl: './block.component.scss'
@@ -16,6 +18,15 @@ export class BlockComponent{
   @Input() block!: Block;
   @Input() module : Modul[] = []
   @Input() blockManager : BlockManager | undefined;
+
+  constructor(private pomodoroSignalService : PomodoroSignalService) {
+    // Lauscht auf Signaländerungen
+    effect(() => {
+      if (this.pomodoroSignalService.pomodoroSignal() === 1) {
+        this.setPomodoroValues()
+      }
+    });
+  }
 
   setModulOfBlock(event : Event): void {
     const select = event.target as HTMLSelectElement;
@@ -48,5 +59,27 @@ export class BlockComponent{
 
   removeBlock(index : number) {
     this.blockManager?.removeBlock(index)
+  }
+
+  /**
+   * Setzt die Lernzeit auf 25 Minuten und die Pausenzeit auf 5 Minuten für alle Blöcke, wenn sich das Signal im PomodoroSignalService ändert.
+   */
+  setPomodoroValues() {
+    const selects = ["lernzeit-select", "pausen-select"];
+
+    for (let selectId of selects) {
+      const selectHtmlElement = document.getElementsByClassName(selectId);
+      for (let i = 0; i < selectHtmlElement.length; i++) {
+        const select = selectHtmlElement[i] as HTMLSelectElement;
+        for (let j = 0; j < select.options.length; j++) {
+          const option = select.options[j];
+          if (selectId === selects[0]) {
+            option.selected = parseInt(option.value) === 25;
+          } else if (selectId === selects[1]) {
+            option.selected = parseInt(option.value) === 5;
+          }
+        }
+      }
+    }
   }
 }
