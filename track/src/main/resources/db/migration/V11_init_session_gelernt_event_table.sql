f56e9e80e1030ab07d80164fb7c2fff53ea3616a
Timo Neske <neske.timo93@gmail.com>
2025-09-17 13:54:57 +0200

    update script

--- track/src/main/resources/db/migration/V11_init_session_gelernt_event_table.sql
+++ track/src/main/resources/db/migration/V11_init_session_gelernt_event_table.sql
@@ -8,7 +8,7 @@ create table session_beendet_event(
 
 create table session_bewertung(
     session_beendet_event int references session_beendet_event(id),
-    konzentration_bewertung decimal,
-    produktivitaet_bewertung decimal,
-    schwierigkeit_bewertung decimal
+    konzentration_bewertung decimal check (konzentration_bewertung between 0 and 10),
+    produktivitaet_bewertung decimal check (produktivitaet_bewertung between 0 and 10),
+    schwierigkeit_bewertung decimal check (schwierigkeit_bewertung between 0 and 10)
 );
