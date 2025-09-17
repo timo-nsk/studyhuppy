b1a4087099008730fb1362228e1e029532c8d3ef
Timo Neske <neske.timo93@gmail.com>
2025-09-17 14:10:45 +0200

    update script

--- track/src/main/resources/db/migration/V11__init_session_gelernt_event_table.sql
+++ track/src/main/resources/db/migration/V11__init_session_gelernt_event_table.sql
@@ -7,7 +7,7 @@ create table session_beendet_event(
 );
 
 create table session_bewertung(
-    session_beendet_event int references session_beendet_event(id),
+    session_beendet_event int references session_beendet_event(id) on delete cascade,
     konzentration_bewertung decimal check (konzentration_bewertung between 0 and 10),
     produktivitaet_bewertung decimal check (produktivitaet_bewertung between 0 and 10),
     schwierigkeit_bewertung decimal check (schwierigkeit_bewertung between 0 and 10)
