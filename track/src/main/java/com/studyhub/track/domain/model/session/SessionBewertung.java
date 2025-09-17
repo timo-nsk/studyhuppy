c5b007b920279c624da1a74f58a9ad82fa4cde1e
Timo Neske <neske.timo93@gmail.com>
2025-09-17 14:10:58 +0200

    add annotation

--- track/src/main/java/com/studyhub/track/domain/model/session/SessionBewertung.java
+++ track/src/main/java/com/studyhub/track/domain/model/session/SessionBewertung.java
@@ -1,9 +1,10 @@
 package com.studyhub.track.domain.model.session;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-
+@AllArgsConstructor
 public class SessionBewertung {
 	Double konzentrationBewertung;
 	Double produktivitaetBewertung;
