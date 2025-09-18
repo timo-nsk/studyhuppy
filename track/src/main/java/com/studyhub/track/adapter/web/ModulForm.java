02d26141561a143b4136d7a67718ce39958794df
Timo Neske <neske.timo93@gmail.com>
2025-09-18 23:56:30 +0200

    remove unused variable and comments

--- track/src/main/java/com/studyhub/track/adapter/web/ModulForm.java
+++ track/src/main/java/com/studyhub/track/adapter/web/ModulForm.java
@@ -1,15 +1,11 @@
 package com.studyhub.track.adapter.web;
 
-import com.studyhub.track.application.service.TimeConverter;
 import com.studyhub.track.domain.model.modul.Kreditpunkte;
 import com.studyhub.track.domain.model.modul.Modul;
 import com.studyhub.track.domain.model.semester.Semester;
 import com.studyhub.track.domain.model.semester.SemesterTyp;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
-
 
 public record ModulForm(
 		String name,
@@ -36,42 +32,9 @@ public record ModulForm(
 {
 	public Modul newModulFromFormData(ModulForm modulForm, String username, int semester) {
 		Kreditpunkte kreditpunkte = new Kreditpunkte(modulForm.creditPoints(), modulForm.kontaktzeitStunden(), modulForm.selbststudiumStunden());
-		TimeConverter tc = new TimeConverter();
-		LocalDateTime actualKlausurDatum = null;
-
-		if (klausurDatum != null) {
-			actualKlausurDatum = LocalDateTime.of(modulForm.klausurDatum(), tc.getLocalTimeFromString(time));
-		}
+		//TODO: Semester objekt erstellen
 		return new Modul(UUID.randomUUID(), name, 0, kreditpunkte, username, true, semester, new Semester(), null);
 	}
-
-	/** OLD METHOD
-	 public Modul newModulFromFormData(ModulForm modulForm, int semesterstufe, String token) {
-	 JWTService jwtService = new JWTService();
-	 //TODO: Semester objekt erstellen
-	 Lerntage lerntage = new Lerntage(mondays, tuesdays, wednesdays, thursdays, fridays, saturdays, sundays, SemesterPhase.VORLESUNG);
-
-
-	 LocalDateTime actualKlausurDatum = null;
-
-	 if (klausurDatum != null) {
-	 actualKlausurDatum = LocalDateTime.of(modulForm.klausurDatum(), TimeConverter.getLocalTimeFromString(time));
-	 }
-
-
-
-	 Kreditpunkte kreditpunkte = new Kreditpunkte(modulForm.creditPoints(), modulForm.kontaktzeitStunden(), modulForm.selbststudiumStunden());
-
-
-	 Semester semester = null;
-
-	 if(semesterCheckbox) {
-	 SemesterTyp semesterPhase = decideSemesterTyp(semesterBeginn);
-	 semester = new Semester(null, semesterPhase, vlBeginn, vlEnde, semesterBeginn, semesterEnde);
-	 }
-
-	 return new Modul(UUID.randomUUID(), modulForm.name(), 0, kreditpunkte, jwtService.extractUsername(token),true, semesterstufe, semester, actualKlausurDatum, lerntage);
-	 } **/
 
 	public SemesterTyp decideSemesterTyp(LocalDate semesterBeginn) {
 		if(semesterBeginn.isAfter(LocalDate.of(LocalDate.now().getYear(), 2, 1)) && semesterBeginn.isBefore(LocalDate.of(LocalDate.now().getYear(), 8, 1))) {
