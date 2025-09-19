0481a5ccc6ebb8cac9d02770f81597e9fa647011
Timo Neske <neske.timo93@gmail.com>
2025-09-19 22:03:21 +0200

    add service

---
+++ track/src/main/java/com/studyhub/track/domain/service/LernplanAktivierungsService.java
@@ -0,0 +1,23 @@
+package com.studyhub.track.domain.service;
+
+import com.studyhub.track.application.service.LernplanRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class LernplanAktivierungsService {
+
+	private final LernplanRepository lernplanRepository;
+
+	public LernplanAktivierungsService(LernplanRepository lernplanRepository) {
+		this.lernplanRepository = lernplanRepository;
+	}
+
+	@Transactional
+	public void setActiveLernplan(UUID fachId, String username) {
+		lernplanRepository.deactivateAllByUsername(username);
+		lernplanRepository.setIsActiveOfLernplan(fachId, true);
+	}
+}
