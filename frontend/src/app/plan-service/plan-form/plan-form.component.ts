e6abef2db9c83e6b63857d724492454147856708
Timo Neske <neske.timo93@gmail.com>
2025-09-20 14:08:50 +0200

    create lernplan-form component

---
+++ frontend/src/app/plan-service/plan-form/plan-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-form',
+  imports: [],
+  templateUrl: './plan-form.component.html',
+  styleUrl: './plan-form.component.scss'
+})
+export class PlanFormComponent {
+
+}
