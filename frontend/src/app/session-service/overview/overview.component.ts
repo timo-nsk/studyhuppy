cf6884c61e0c0e1ceca6b3294439a9f2371eff92
Timo Neske <neske.timo93@gmail.com>
2025-09-24 22:03:59 +0200

    implement overview page

--- frontend/src/app/session-service/overview/overview.component.ts
+++ frontend/src/app/session-service/overview/overview.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {SessionApiService} from '../session-api.service';
+import {Session} from '../session-domain';
+import {SnackbarService} from '../../snackbar.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [NgForOf, NgIf, RouterLink],
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.scss'
+  styleUrls: ['./overview.component.scss', '../../general.scss', '../../button.scss']
 })
-export class SessionOverviewComponent {
+export class SessionOverviewComponent implements OnInit{
+  sessionApiService = inject(SessionApiService);
+  snackbarService = inject(SnackbarService)
+  lernsessions : Session[] | undefined
 
+  ngOnInit(): void {
+    this.sessionApiService.getSessions().subscribe({
+      next: (data : Session[]) =>{
+        this.lernsessions = data
+      },
+      error: err => {
+        console.error("Error fetching sessions", err);
+      }
+    })
+  }
+
+  deleteSession(lernsessionId: string | undefined, titel: string) {
+    this.sessionApiService.deleteSession(lernsessionId).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.ngOnInit()
+        this.snackbarService.openSuccess(`Lernsession "${titel}" gelöscht`)
+      },
+      error: (error) => {
+        console.error('Error deleting lernsession:', error);
+        this.snackbarService.openError('Fehler beim Löschen der Lernsession');
+      }
+    })
+  }
+
+  hasLernsessions() : boolean {
+    return this.lernsessions !== undefined && this.lernsessions.length > 0
+  }
 }
