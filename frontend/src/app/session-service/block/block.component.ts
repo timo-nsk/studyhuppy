55f5c54a80e1bd57e10d5fc2d779c5593e78d873
Timo Neske <neske.timo93@gmail.com>
2025-09-27 21:38:38 +0200

    add import NgIf

--- frontend/src/app/session-service/block/block.component.ts
+++ frontend/src/app/session-service/block/block.component.ts
@@ -1,5 +1,5 @@
 import {Component, effect, Input} from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {Modul} from '../../modul-service/module/domain';
 import {FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {PomodoroSignalService} from '../create/pomodoro-signal.service';
@@ -7,7 +7,7 @@ import {BlockFormManager} from './blockform-manager.service';
 
 @Component({
   selector: 'Lernblock',
-  imports: [NgForOf, FormsModule, ReactiveFormsModule],
+  imports: [NgForOf, FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './block.component.html',
   standalone: true,
   styleUrl: './block.component.scss'
