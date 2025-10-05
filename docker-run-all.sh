5196428b7183cbf987fafe5578dea4fd69076aa3
Timo Neske <Timo.Neske@hhu.de>
2025-10-05 16:06:26 +0200

    update script

--- docker-run-all.sh
+++ docker-run-all.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Studyhub-Services..."
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Mail-Service..."
-docker compose --env-file mail/.env -f mail/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Mail-Service gestartet."
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Kartei-Service..."
-docker compose --env-file kartei/.env -f kartei/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Kartei-Service gestartet."
+STACK_NAME="studyhuppy"
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Track-Service..."
-docker compose --env-file track/.env -f track/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Track-Service gestartet."
+echo "[$current_time] Starte alle Studyhuppy-Services unter Stack \"$STACK_NAME\"..."
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Mindmap-Service..."
-docker compose --env-file mindmap/.env -f mindmap/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Mindmap-Service gestartet."
+echo "Starte Mail-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/mail/.env -f backend/mail/docker-compose.yaml up -d --build
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Authentication-Service..."
-docker compose --env-file authentication/.env -f authentication/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Authentication-Service gestartet."
+echo "Starte Kartei-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/kartei/.env -f backend/kartei/docker-compose.yaml up -d --build
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Starte Actuator-Service..."
-docker compose --env-file actuator/.env -f actuator/docker-compose.yaml up -d --build
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Actuator-Service gestartet."
+echo "Starte Track-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/track/.env -f backend/track/docker-compose.yaml up -d --build
 
-echo "[$(date '+%d.%m.%Y %H:%M:%S')] Alle Studyhub-Services sind gestartet."
+echo "Starte Mindmap-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/mindmap/.env -f backend/mindmap/docker-compose.yaml up -d --build
+
+echo "Starte Authentication-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/authentication/.env -f backend/authentication/docker-compose.yaml up -d --build
+
+echo "Starte Actuator-Service..."
+docker compose -p "$STACK_NAME" --env-file backend/actuator/.env -f backend/actuator/docker-compose.yaml up -d --build
+
+echo "Starte Frontend..."
+docker compose -p "$STACK_NAME" -f frontend/docker-compose.yaml up -d --build
+
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[$current_time] Alle Studyhuppy-Services unter Stack \"$STACK_NAME\" gestartet."
