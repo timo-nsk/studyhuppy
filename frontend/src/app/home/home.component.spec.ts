a3b003a8bb40228a9e407c203af1662d07ee8283
Timo Neske <neske.timo93@gmail.com>
2025-10-09 22:11:29 +0200

    fix test

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {HttpHeaders, provideHttpClient} from '@angular/common/http';
+import {HeaderService} from '../header.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,17 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [{
+        provide: HeaderService,
+        useValue: {
+          createAuthHeader: () => new HttpHeaders({
+            Authorization: 'Bearer MOCK_TOKEN',
+            'Content-Type': 'application/json'
+          })
+        }
+      },
+      provideHttpClient()]
     })
     .compileComponents();
 
