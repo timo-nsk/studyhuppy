0e4424fc7481353f0a41b247c35989fafd26c6f8
Timo Neske <neske.timo93@gmail.com>
2025-10-09 22:07:58 +0200

    fix test

--- frontend/src/app/kartei-service/kartei-service.component.spec.ts
+++ frontend/src/app/kartei-service/kartei-service.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { KarteiServiceComponent } from './kartei-service.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {ActivatedRoute} from '@angular/router';
-import {of} from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HeaderService } from '../header.service'; // Pfad ggf. anpassen
+import { HttpHeaders } from '@angular/common/http';
 
 describe('KarteiServiceComponent', () => {
   let component: KarteiServiceComponent;
@@ -11,20 +12,33 @@ describe('KarteiServiceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [KarteiServiceComponent, HttpClientTestingModule],
-      providers: [{
-        provide: ActivatedRoute,
-        useValue: {
-          snapshot: {
-            paramMap: {
-              get: (key: string) => '123',
+      imports: [
+        KarteiServiceComponent,
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '123',
+              },
             },
+            params: of({ id: '123' }),
           },
-          params: of({ id: '123' }),
         },
-      }]
-    })
-    .compileComponents();
+        {
+          provide: HeaderService,
+          useValue: {
+            createAuthHeader: () => new HttpHeaders({
+              Authorization: 'Bearer MOCK_TOKEN',
+              'Content-Type': 'application/json'
+            })
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(KarteiServiceComponent);
     component = fixture.componentInstance;
