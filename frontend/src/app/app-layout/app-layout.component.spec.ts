22f43797504c32563e945dd9f17e3dcb93796255
Timo Neske <neske.timo93@gmail.com>
2025-10-09 23:24:08 +0200

    fix test

--- frontend/src/app/app-layout/app-layout.component.spec.ts
+++ frontend/src/app/app-layout/app-layout.component.spec.ts
@@ -5,12 +5,21 @@ import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {AuthApiService} from '../auth-service/auth.service';
 import {ActivatedRoute} from '@angular/router';
 import {of} from 'rxjs';
+import {HeaderService} from '../header.service';
+import {HttpHeaders} from '@angular/common/http';
 
 describe('AppLayoutComponent', () => {
   let component: AppLayoutComponent;
   let fixture: ComponentFixture<AppLayoutComponent>;
 
   beforeEach(async () => {
+    const dummyToken = [
+      btoa(JSON.stringify({ alg: 'HS256', typ: 'JWT' })),
+      btoa(JSON.stringify({ sub: 'testuser', authorities: ['ROLE_ADMIN'] })),
+      'signature'
+    ].join('.');
+    localStorage.setItem('auth_token', dummyToken);
+
     await TestBed.configureTestingModule({
       imports: [AppLayoutComponent, HttpClientTestingModule],
       providers: [AuthApiService,
