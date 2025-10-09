344b53416b345c957d1f202bd0e8f363db119359
Timo Neske <neske.timo93@gmail.com>
2025-10-09 22:59:43 +0200

    fix test

--- frontend/src/app/auth-service/admin-service/admin-service.component.spec.ts
+++ frontend/src/app/auth-service/admin-service/admin-service.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminServiceComponent } from './admin-service.component';
+import {HttpHeaders, provideHttpClient} from '@angular/common/http';
+import {HeaderService} from '../../header.service';
 
 describe('AdminServiceComponent', () => {
   let component: AdminServiceComponent;
@@ -8,7 +10,17 @@ describe('AdminServiceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminServiceComponent]
+      imports: [AdminServiceComponent],
+      providers: [{
+        provide: HeaderService,
+        useValue: {
+          createAuthHeader: () => new HttpHeaders({
+            Authorization: 'Bearer MOCK_TOKEN',
+            'Content-Type': 'application/json'
+          })
+        }
+      },
+      provideHttpClient()]
     })
     .compileComponents();
 
