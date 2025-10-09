655f84f35a7ef926602804a83ccb0159bd21b04d
Timo Neske <neske.timo93@gmail.com>
2025-10-09 22:24:54 +0200

    fix test

--- frontend/src/app/plan-service/edit/edit.component.spec.ts
+++ frontend/src/app/plan-service/edit/edit.component.spec.ts
@@ -1,16 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlanEditComponent } from './edit.component';
+import { ActivatedRoute } from '@angular/router';
+import { HttpHeaders, provideHttpClient } from '@angular/common/http';
+import { HeaderService } from '../../header.service';
+import { Lernplan } from '../plan-domain';
 
-describe('EditComponent', () => {
+describe('PlanEditComponent', () => {
   let component: PlanEditComponent;
   let fixture: ComponentFixture<PlanEditComponent>;
 
+  const mockLernplan: Lernplan = {
+    fachId: 'uuid',
+    username: 'a',
+    titel: 't',
+    tagesListe: [],
+    active: false
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlanEditComponent]
-    })
-    .compileComponents();
+      imports: [PlanEditComponent],
+      providers: [
+        provideHttpClient(),
+        {
+          provide: HeaderService,
+          useValue: {
+            createAuthHeader: () =>
+              new HttpHeaders({
+                Authorization: 'Bearer MOCK_TOKEN',
+                'Content-Type': 'application/json',
+              }),
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: { lernplanToEdit: mockLernplan }
+            }
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PlanEditComponent);
     component = fixture.componentInstance;
@@ -19,5 +50,9 @@ describe('EditComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have lernplanToEdit set from resolver', () => {
+    expect(component.lernplanToEdit).toEqual(mockLernplan);
   });
 });
