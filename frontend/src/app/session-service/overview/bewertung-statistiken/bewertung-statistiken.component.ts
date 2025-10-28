4aa39235799a419ddf1b0c1ff12b912345f40ce0
Timo Neske <neske.timo93@gmail.com>
2025-10-28 19:53:29 +0100

    Update html select dropdown to have the values of the sessionInfo object

--- frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
+++ frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
@@ -2,11 +2,15 @@ import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {SessionApiService, SessionBewertungGeneralStatistik} from '../../session-api.service';
 import {SessionInfoDto} from '../../session-domain';
+import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-bewertung-statistiken',
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './bewertung-statistiken.component.html',
   styleUrl: './bewertung-statistiken.component.scss'
@@ -15,6 +19,7 @@ export class SessionBewertungStatistikenComponent implements OnInit{
   sessionApiService : SessionApiService = inject(SessionApiService);
   generalStatistiken : SessionBewertungGeneralStatistik = {} as SessionBewertungGeneralStatistik
   sessionInfo : SessionInfoDto[] = {} as SessionInfoDto[];
+  selectedSessionId!: string;
 
   ngOnInit(): void {
     this.sessionApiService.getGeneralBewertungStatistiken().subscribe({
@@ -26,6 +31,7 @@ export class SessionBewertungStatistikenComponent implements OnInit{
     this.sessionApiService.getLernplanSessionData().subscribe({
       next: data => {
         this.sessionInfo = data;
+        this.selectedSessionId = this.sessionInfo[0].fachId
         console.log("Fetched general statistik: ", this.sessionInfo);
       }
     })
