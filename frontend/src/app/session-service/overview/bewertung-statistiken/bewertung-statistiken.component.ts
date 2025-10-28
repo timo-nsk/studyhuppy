9715be5e66a16e812696bc2464e9d937f7d97f37
Timo Neske <neske.timo93@gmail.com>
2025-10-28 19:39:42 +0100

    Add field userSessionIds to fetch all the ids of created sessions by a user and implement api call method.

--- frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
+++ frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {SessionApiService, SessionBewertungGeneralStatistik} from '../../session-api.service';
+import {SessionApiService, SessionBewertungGeneralStatistik, SessionIds} from '../../session-api.service';
 
 @Component({
   selector: 'app-bewertung-statistiken',
@@ -13,12 +13,19 @@ import {SessionApiService, SessionBewertungGeneralStatistik} from '../../session
 export class SessionBewertungStatistikenComponent implements OnInit{
   sessionApiService : SessionApiService = inject(SessionApiService);
   generalStatistiken : SessionBewertungGeneralStatistik = {} as SessionBewertungGeneralStatistik
+  userSessionIds : SessionIds = {} as SessionIds;
 
   ngOnInit(): void {
     this.sessionApiService.getGeneralBewertungStatistiken().subscribe({
       next: data => {
         this.generalStatistiken = data;
         console.log("Fetched general statistik: ", this.generalStatistiken);
+      }
+    });
+    this.sessionApiService.getUserSessionIds().subscribe({
+      next: data => {
+        this.userSessionIds = data;
+        console.log("Fetched general statistik: ", this.userSessionIds);
       }
     });
   }
