7cb472499b23f181cd88b8df03dc834858e1e3d4
Timo Neske <neske.timo93@gmail.com>
2025-10-28 17:32:02 +0100

    Update styling and html structure

--- frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
+++ frontend/src/app/session-service/overview/bewertung-statistiken/bewertung-statistiken.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-bewertung-statistiken',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './bewertung-statistiken.component.html',
   styleUrl: './bewertung-statistiken.component.scss'
 })
