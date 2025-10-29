cc12417fe4abecd4054d034d0041d898cee3f1d4
Timo Neske <Timo.Neske@hhu.de>
2025-10-29 22:23:14 +0100

    Add java doc

--- backend/track/src/main/java/com/studyhub/track/domain/model/session/Block.java
+++ backend/track/src/main/java/com/studyhub/track/domain/model/session/Block.java
@@ -4,6 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.util.UUID;
 
+/**
+ * A Block is part of a Session and holds all information needed for learning a module.
+ *
+ * <ul>
+ *     <li>{@code fachId} - Unique ID of the Block</li>
+ *     <li>{@code modulId} - ID of the module that is being learned</li>
+ *     <li>{@code modulName} - Name of the module</li>
+ *     <li>{@code lernzeitSeconds} - The duration the module is studied in seconds</li>
+ *     <li>{@code pausezeitSeconds} - The pause duration between learning periods</li>
+ * </ul>
+ */
 @Data
 @AllArgsConstructor
 public class Block {
--- backend/track/src/main/java/com/studyhub/track/domain/model/session/SessionBewertung.java
+++ backend/track/src/main/java/com/studyhub/track/domain/model/session/SessionBewertung.java
@@ -3,6 +3,15 @@ package com.studyhub.track.domain.model.session;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Contains information about the users subjective rating of their learning sessions.
+ *
+ * <ul>
+ *     <li>{@code konzentrationBewertung} - Rating of the users ability to hold concentration during the session</li>
+ *     <li>{@code produktivitaetBewertung}  - Rating of the users feeling of productivity during the session</li>
+ *     <li>{@code schwierigkeitBewertung}  - Rating of how difficult the topics of the session were</li>
+ * </ul>
+ */
 @Data
 @AllArgsConstructor
 public class SessionBewertung {
