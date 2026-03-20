61dac25dbf06dc689776be5156e999e27ad71208
Timo Neske <neske.timo93@gmail.com>
2026-03-20 16:29:58 +0100

    Update config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|mjs|html)$': 'jest-preset-angular'
-  },
-  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testEnvironment: 'jsdom'
 };
